2cb90b38980d58d606f0299aed1fdd4a4e746a5a
Sharp0802 <sharp0802.business@gmail.com>
2022-08-16 22:42:20 +0900

    Impl wrapper for method

---
+++ src/DynamicInvoker/DynamicInvoker/MethodCaller.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+
+namespace DynamicInvoker;
+
+/// <summary>
+/// Wrapper class for method with reflection.
+/// </summary>
+public class MethodCaller : Caller
+{
+    /// <summary>
+    /// Create dynamic wrapper for method.
+    /// </summary>
+    /// <param name="type"><see cref="System.Type"/> that contains <paramref name="method"/></param>
+    /// <param name="method">Specific <see cref="System.Reflection.MethodInfo"/> to wrap.</param>
+    public static MethodCaller Create(Type type, MethodInfo method)
+    {
+        return new MethodCaller(method, type);
+    }
+    
+    private MethodCaller(MethodInfo method, Type type)
+    {
+        var dynMethod = CreateDynamicMethod(method, type);
+        Delegate = CreateDelegate(dynMethod);
+    }
+
+    private DynamicDelegate Delegate { get; }
+    
+    /// <summary>
+    /// Call the method.
+    /// </summary>
+    /// <param name="target">Instance that contains this method.</param>
+    /// <param name="args">The arguments of method.</param>
+    /// <returns>If return type of the method is <see cref="System.Void"/>, null. otherwise, return value of the method.</returns>
+    public object? Call(object? target, object?[] args) => Delegate.Invoke(target, args);
+}
